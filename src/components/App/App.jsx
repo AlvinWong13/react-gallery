2628a57f5cd2151818fe3b63e583c1f02d658a46
Alvin Wong <wong.alvin13@gmail.com>
2021-02-19 23:12:09 -0600

    get and put added to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import './App.css';
 
+import{ useState } from 'react';
+import axios from 'axios';
+
+let [galleryList, setGalleryList] = userState([]);
+
+const getGallery = () => {
+  axios.get('/gallery')
+  .then(response => {
+    setGalleryList(response.data);
+  })
+  .catch(err => {
+    console.log('Unable to retrieve gallery', err);
+    res.sendStatus(500)
+  })
+}
+
+const increaseLikes = (event) => {
+  const galleryId = event.target.dataset.id;
+  const galleryLike = event.target.dataset.likes;
+  axios({
+    url: `/gallery/${galleryId}`,
+    method: 'PUT',
+    data: {
+      galleryLike
+    }
+  })
+  .then(response => {
+    console.log('Increased likes', response);
+    getGallery();
+  })
+  .catch(err => {
+    console.log('Unable to like photo', err);
+  })
+}
+
 function App() {
     return (
       <div className="App">
